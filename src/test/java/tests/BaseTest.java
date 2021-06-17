66221bac266a52640acb04a963a4900d6736fe54
Alexander Gordeichik <a.gordeichik@invento.by>
2021-06-17 13:51:32 +0300

    soucedemo 2

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -34,10 +34,10 @@ public class BaseTest implements ITestConstants {
 //        if (System.getProperty("browser").equals("chrome")) {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-//        options.addArguments("disable-infobars"); // disabling infobars
-//        options.addArguments("--disable-extensions"); // disabling extensions
-//        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
 //      DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
